441c94c475c7071495088826d10f2ac226e96107
Ernest <emudjambere@gmail.com>
2024-03-13 00:01:58 -0400

    Introducing new project and Email feature by using dependency injection.

---
+++ FriendsTown.Transversal/EmailService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendsTown.Transversal
+{
+    public class EmailService : IEmailService
+    {
+        public void SendEmail(string from, string to, string subject, string message)
+        {
+
+            MailMessage mailMessage = new MailMessage(from, to, subject, message);
+            mailMessage.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+            smtpClient.PickupDirectoryLocation = @"C:\Email.FriendsTown";
+            smtpClient.UseDefaultCredentials = true;
+
+            smtpClient.Send(mailMessage);
+        }
+    }
+}
